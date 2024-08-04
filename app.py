09fb7abe6e33749960c958ca4068c9fccb04c767
zahar <zaharalex25@gmail.com>
2024-08-04 14:46:38 +0200

    chore: Refactor app.py for improved query engine configuration

--- app.py
+++ app.py
@@ -44,7 +44,14 @@ b = "geben Sie bitte die Knauf System ID an, die diesen Eigenschaften entspricht
 
 @app.post("/api/search", response_model=Response, status_code=200)
 def search(query: Query):
-    query_engine = index.as_query_engine(vector_store_query_mode="hybrid", output=Response, response_mode="tree_summarize", verbose=True)
+    query_engine = index.as_query_engine(
+        vector_store_query_mode="hybrid", 
+        # similarity_top_k=query.similarity_top_k, #Weird behavior
+        alpha=0.5,
+        output=Response, 
+        response_mode="tree_summarize", 
+        verbose=True
+    )
     response = query_engine.query(a + query.query + b)
     print("response", response)
     print("response.source_nodes", response.source_nodes)
