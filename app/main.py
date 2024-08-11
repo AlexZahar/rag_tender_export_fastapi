b6afaa2996041bc91a88e2e91e6355ff44efd31a
zahar <zaharalex25@gmail.com>
2024-08-11 12:27:12 +0200

    Updated the RAG class with query engine method configuration

--- app/main.py
+++ app/main.py
@@ -3,52 +3,38 @@ from services.rag_service import RAG
 from config.settings import load_config
 from services.telemetry_service import setup_telemetry
 from models.models import Query, Response, SourceNode
-from llama_index.core.postprocessor import SentenceTransformerRerank
-from llama_index.core.indices.query.query_transform.base import (
-    HyDEQueryTransform,
-)
-from llama_index.core.query_engine import TransformQueryEngine
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core import PromptTemplate
-from llama_index.llms.openai import OpenAI
-from services.query_parser_service import generate_queries
-
 
 setup_telemetry()
-config=load_config()
+config = load_config()
 
-rerank = SentenceTransformerRerank(
-    model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=3
-)
 rag = RAG(config_file=config)
 index = rag.milvus_index()
-hyde = HyDEQueryTransform(include_original=True)
 
 app = FastAPI()
-embed_model = HuggingFaceEmbedding(model_name=config["embedding_model"], trust_remote_code=True) 
 
 @app.get("/")
 def root():
     return {"message": "Research RAG"}
 
-a = "Basierend auf den folgenden Eigenschaften:"
-b = "geben Sie bitte die Knauf System ID an, die diesen Eigenschaften entspricht."
-
 @app.post("/api/search", response_model=Response, status_code=200)
 def search(query: Query):
-    queries = generate_queries(query.query, llm)
-    query_engine = index.as_query_engine(
-        vector_store_query_mode="hybrid", 
+    # Format the query
+    parsed_query = rag.parse_query(query, num_queries=1)[0]  # Assuming we want the first generated query
+
+    # Generate the final query
+    final_query = rag.generate_final_query(parsed_query)
+
+    # Create the query engine
+    query_engine = rag.query_engine(
+        index,
+        rerank=True,
         similarity_top_k=query.similarity_top_k,
-        alpha=0.5,
-        output=Response, 
-        node_postprocessors=[rerank],
-        response_mode="tree_summarize", 
-        verbose=True,
-        embedding=embed_model
+        response_mode="tree_summarize",
+        hydeTransform=True
     )
-    query_engine = TransformQueryEngine(query_engine, query_transform=hyde)
-    response = query_engine.query(a + query.query + b)
+
+    # Execute the query
+    response = query_engine.query(final_query)
 
     # Create a list of SourceNode objects
     source_nodes = [
--- services/rag_service.py
+++ services/rag_service.py
@@ -4,8 +4,13 @@ from llama_index.vector_stores.milvus import MilvusVectorStore
 from llama_index.core import Settings
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.llms.openai import OpenAI
+from llama_index.core.postprocessor import SentenceTransformerRerank
+from llama_index.core.query_engine import TransformQueryEngine
+from llama_index.core.indices.query.query_transform.base import (
+    HyDEQueryTransform,
+)
 
-
+from services.query_parser_service import generate_queries
 class RAG:
     def __init__(self, config_file):
         self.config = config_file
@@ -31,3 +36,42 @@ class RAG:
             vector_store=milvus_vector_store
         )
         return index
+    
+    def query_engine(self, index, rerank=True, alpha=0.5, similarity_top_k=5, response_mode="tree_summarize", hydeTransform=False):
+        node_postprocessors = []
+        hyde = HyDEQueryTransform(include_original=True)
+
+        if rerank:
+            rerank_processor = SentenceTransformerRerank(model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=3)
+            node_postprocessors.append(rerank_processor)
+        
+        query_engine = index.as_query_engine(
+            vector_store_query_mode="hybrid", 
+            similarity_top_k=similarity_top_k,
+            alpha=alpha,
+            node_postprocessors=node_postprocessors,
+            response_mode=response_mode, 
+            verbose=True
+        )
+        if hydeTransform:
+            query_engine = TransformQueryEngine(query_engine, query_transform=hyde)
+
+        return query_engine
+    
+    def generate_final_query(self, query):
+        a = "Basierend auf den folgenden Eigenschaften:"
+        b = "</br> Geben Sie bitte die Knauf System ID an, die diesen Eigenschaften entspricht."
+        return a + query + b
+    
+    def get_llm(self):
+        llm = OpenAI(model=self.config["llm_name"])
+        return llm
+    
+    def get_embed_model(self):
+        embed_model = HuggingFaceEmbedding(model_name=self.config["embedding_model"], trust_remote_code=True)
+        return embed_model
+
+    def parse_query(self, query, num_queries=1):
+        llm = self.get_llm()
+        queries = generate_queries(query.query, llm, num_queries)
+        return queries
